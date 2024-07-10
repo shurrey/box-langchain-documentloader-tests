5cc34ef38504820d6c2bfb6e8298351917ae135a
Scott Hurrey <scott.hurrey@gmail.com>
2024-07-10 11:14:41 -0400

    improve readability of search tool test

--- tool_test_scripts/test_box_file_search_tool.py
+++ tool_test_scripts/test_box_file_search_tool.py
@@ -2,10 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from langchain_community.tools.box.box_file_search import BoxFileSearchTool
-from langchain_community.tools.box.box_text_rep import BoxTextRepTool
-
 from langchain_core.messages import HumanMessage
-
 from langchain_openai import ChatOpenAI
 
 from langgraph.prebuilt import create_react_agent
@@ -14,9 +11,8 @@ load_dotenv("../config/.token.env")
 load_dotenv("../config/.box.env")
 load_dotenv("../config/.openai.env")
 
-openai_key = os.getenv("OPENAI_API_KEY")
-
-box_developer_token=os.getenv("BOX_DEVELOPER_TOKEN")
+os.environ["OPENAI_API_KEY"]
+os.environ["BOX_DEVELOPER_TOKEN"]
 box_search_query=os.getenv("BOX_SEARCH_QUERY")
 
 box_search_tool = BoxFileSearchTool()
@@ -27,8 +23,15 @@ model = ChatOpenAI(model="gpt-4")
 
 agent_executor = create_react_agent(model, tools)
 
+prompt = (
+    f"find a document containing the {box_search_query}. If there are multiple documents returned,"
+     "choose only the first one. Then create a script for a voiceover based on the document for a 30"
+     "second commercial. The content of this commercial should entice a viewer to want to see the movie."
+     "The voice should be that of a professional voice over actor, and the tone should be excited."
+)
+
 for chunk in agent_executor.stream(
-    {"messages": [HumanMessage(content=f"find a document containing the {box_search_query}. If there are multiple documents returned, choose only the first one. Then create a script for a voiceover based on the document for a 30 second commercial. The content of this commercial should entice a viewer to want to see the movie. The voice should be that of a professional voice over actor, and the tone should be excited.")]}
+    {"messages": [HumanMessage(content=prompt)]}
 ):
     print(chunk)
     print("----")
